1008ee6e3070ebf3e1dfd76286fa9f866508078b
IlyaMurashov <murashov.ilya@gmail.com>
2017-07-01 15:04:55 +0200

    Deque API

---
+++ queues/src/main/java/Deque.java
@@ -0,0 +1,35 @@
+import java.util.Iterator;
+
+public class Deque<T> implements Iterable<T> {
+    public Deque() {}
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void addFirst(T item) {
+
+    }
+
+    public void addLast(T item) {
+
+    }
+
+    public T removeFirst() {
+       return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+
+}
