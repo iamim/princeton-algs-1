50434eb433f6876f71c2d06c64c01cd31fcda808
IlyaMurashov <murashov.ilya@gmail.com>
2017-08-05 15:23:46 +0200

    First submit

--- a-star/src/main/java/Solver.java
+++ a-star/src/main/java/Solver.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.MinPQ;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 
 public class Solver {
     private final static Comparator<SearchNode> MANHATTAN_COMPARATOR = Comparator.comparingInt(
@@ -12,36 +13,69 @@ public class Solver {
     private final MinPQ<SearchNode> mainPQ = new MinPQ<>(MANHATTAN_COMPARATOR);
     private final MinPQ<SearchNode> twinPQ = new MinPQ<>(MANHATTAN_COMPARATOR);
 
-    private boolean initialIsSolveable;
+    private final boolean initialIsSolvable;
+    private final LinkedList<Board> solution = new LinkedList<>();
+
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         boolean mainTurn = true;
+        SearchNode solved = null;
 
         mainPQ.insert(new SearchNode(initial, null, 0));
         twinPQ.insert(new SearchNode(initial.twin(), null, 0));
 
-        if (mainTurn) {
-            makeOneTurn(mainPQ);
+        while (solved == null) {
+            if (mainTurn)
+                solved = tryToSolve(mainPQ);
+            else
+                solved = tryToSolve(twinPQ);
+
+            mainTurn = !mainTurn;
         }
+
+        initialIsSolvable = !mainTurn;
+
+        if (initialIsSolvable) {
+            solution.add(solved.board);
+
+            while (solved.prev != null) {
+                solution.addFirst(solved.prev.board);
+
+                solved = solved.prev;
+            }
+        }
+
     }
 
-    private void makeOneTurn(MinPQ<SearchNode> PQ) {
+    private SearchNode tryToSolve(MinPQ<SearchNode> PQ) {
         SearchNode min = PQ.delMin();
+
+        if (min.board.isGoal()) return min;
+
+        for (Board neighbor : min.board.neighbors()) {
+            if (neighbor.equals(min.prev.board)) continue;
+
+            PQ.insert(new SearchNode(neighbor, min, min.movesToHere + 1));
+        }
+
+        return null;
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return initialIsSolveable;
+        return initialIsSolvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves() {
+        if (initialIsSolvable) return solution.size();
         return -1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution() {
+        if (initialIsSolvable) return solution;
         return null;
     }
 
