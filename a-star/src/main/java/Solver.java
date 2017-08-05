d2b25b962f089b450b95065251d91b0ddadbea16
IlyaMurashov <murashov.ilya@gmail.com>
2017-08-05 14:34:47 +0200

    Implemented Board#equals

--- a-star/src/main/java/Solver.java
+++ a-star/src/main/java/Solver.java
@@ -9,15 +9,30 @@ public class Solver {
     private final static Comparator<SearchNode> HAMMING_COMPARATOR = Comparator.comparingInt(
             n -> (n.movesToHere + n.board.hamming()));
 
-    private final MinPQ<SearchNode> mainPQ = new MinPQ<>();
-    private final MinPQ<SearchNode> twinPQ = new MinPQ<>();
+    private final MinPQ<SearchNode> mainPQ = new MinPQ<>(MANHATTAN_COMPARATOR);
+    private final MinPQ<SearchNode> twinPQ = new MinPQ<>(MANHATTAN_COMPARATOR);
+
+    private boolean initialIsSolveable;
+
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+        boolean mainTurn = true;
+
+        mainPQ.insert(new SearchNode(initial, null, 0));
+        twinPQ.insert(new SearchNode(initial.twin(), null, 0));
+
+        if (mainTurn) {
+            makeOneTurn(mainPQ);
+        }
+    }
+
+    private void makeOneTurn(MinPQ<SearchNode> PQ) {
+        SearchNode min = PQ.delMin();
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return false;
+        return initialIsSolveable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
@@ -33,8 +48,6 @@ public class Solver {
     // solve a slider puzzle (given below)
     public static void main(String[] args) {
     }
-
-
 
     private class SearchNode{
         private final int movesToHere;
