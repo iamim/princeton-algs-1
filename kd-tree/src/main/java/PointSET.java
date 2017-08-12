3873ccc03e8ca691868abaf3869cc1f3204289e4
IlyaMurashov <murashov.ilya@gmail.com>
2017-08-12 20:31:38 +0200

    Implemented PointSET

---
+++ kd-tree/src/main/java/PointSET.java
@@ -0,0 +1,73 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.TreeSet;
+
+public class PointSET {
+    private TreeSet<Point2D> points;
+
+    // construct an empty set of points
+    public PointSET() {
+        points = new TreeSet<>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return points.isEmpty();
+    }
+
+    // number of points in the set
+    public int size() {
+        return points.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        points.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        return points.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        points.forEach(Point2D::draw);
+    }
+
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+
+        List<Point2D> inside = new LinkedList<>();
+        for (Point2D thisP : points) {
+            if (thisP.x() <= rect.xmax() && thisP.x() >= rect.xmin()
+                    && thisP.y() <= rect.ymax() && thisP.y() >= rect.ymin()) {
+                inside.add(thisP);
+            }
+        }
+
+        return inside;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+
+        Point2D nearest = null;
+        for (Point2D thisP : points) {
+            if (nearest == null) nearest = thisP;
+            else if (p.distanceTo(thisP) < p.distanceTo(nearest)) nearest = thisP;
+        }
+
+        return nearest;
+    }
+
+}
