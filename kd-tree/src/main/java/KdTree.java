1e6f14c584c7a1e23305c05f30aa75c6d7256fd4
IlyaMurashov <murashov.ilya@gmail.com>
2017-08-13 19:24:54 +0200

    KdTree works with KdTreeVisualizer

--- kd-tree/src/main/java/KdTree.java
+++ kd-tree/src/main/java/KdTree.java
@@ -1,11 +1,9 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
-    private enum ORIENT {
-        VERT,
-        HORIZ
-    }
+
     private static boolean VERT = true;
     private static boolean HORIZ = false;
 
@@ -29,28 +27,31 @@ public class KdTree {
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-        if (p == null) throw new IllegalArgumentException();
+        if (p == null)
+            throw new IllegalArgumentException();
 
-        if (contains(p)) return;
+        if (contains(p))
+            return;
 
-        root = insert(VERT, root, p);
+        root = insertHelper(VERT, root, p, 0, 0, 1, 1);
     }
 
-    // If too complicated check for contains first
-    private Node insert(boolean orient, Node parent, Point2D p) {
-        if (parent == null) return new Node(p);
+    private Node insertHelper(boolean orient, Node parent, Point2D p, double xmin, double ymin,
+            double xmax, double ymax) {
+        if (parent == null)
+            return new Node(p, new RectHV(xmin, ymin, xmax, ymax));
 
         if (orient == VERT) {
             if (p.x() < parent.p.x())
-                parent.lb = insert(HORIZ, parent.lb, p);
+                parent.lb = insertHelper(HORIZ, parent.lb, p, xmin, ymin, parent.p.x(), ymax);
             else
-                parent.rt = insert(HORIZ, parent.rt, p);
+                parent.rt = insertHelper(HORIZ, parent.rt, p, parent.p.x(), ymin, xmax, ymax);
         }
         else {
             if (p.y() < parent.p.y())
-                parent.lb = insert(VERT, parent.lb, p);
+                parent.lb = insertHelper(VERT, parent.lb, p, xmin, ymin, xmax, parent.p.y());
             else
-                parent.rt = insert(VERT, parent.rt, p);
+                parent.rt = insertHelper(VERT, parent.rt, p, xmin, parent.p.y(), xmax, ymax);
         }
 
         return parent;
@@ -58,33 +59,62 @@ public class KdTree {
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        if (p == null) throw new IllegalArgumentException();
+        if (p == null)
+            throw new IllegalArgumentException();
 
-        return contains(VERT, root, p);
+        return containsHelper(VERT, root, p);
     }
 
-    private boolean contains(boolean orient, Node parent, Point2D p) {
-        if (parent == null) return false;
+    private static boolean containsHelper(boolean orient, Node parent, Point2D p) {
+        if (parent == null)
+            return false;
 
-        if (parent.p.equals(p)) return true;
+        if (parent.p.equals(p))
+            return true;
 
         if (orient == VERT) {
             if (p.x() < parent.p.x())
-                return contains(HORIZ, parent.lb, p);
+                return containsHelper(HORIZ, parent.lb, p);
             else
-                return contains(HORIZ, parent.rt, p);
+                return containsHelper(HORIZ, parent.rt, p);
         }
         else {
             if (p.y() < parent.p.y())
-                return contains(VERT, parent.lb, p);
+                return containsHelper(VERT, parent.lb, p);
             else
-                return contains(VERT, parent.rt, p);
+                return containsHelper(VERT, parent.rt, p);
         }
     }
 
     // draw all points to standard draw
     public void draw() {
+        drawNode(VERT, root);
+    }
 
+    private static void drawNode(boolean orient, Node node) {
+        if (node == null)
+            return;
+
+        // draw point
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+
+        StdDraw.point(node.p.x(), node.p.y());
+
+        // draw line
+        StdDraw.setPenRadius();
+        if (orient == VERT) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(node.p.x(), node.rect.ymin(), node.p.x(), node.rect.ymax());
+        }
+        else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(node.rect.xmin(), node.p.y(), node.rect.xmax(), node.p.y());
+        }
+
+        // draw children nodes
+        drawNode(!orient, node.lb);
+        drawNode(!orient, node.rt);
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -94,7 +124,44 @@ public class KdTree {
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if (p == null)
+            throw new IllegalArgumentException();
+
+        if (isEmpty())
+            return null;
+
+        return nearestHelper(VERT, root, p, root.p);
+    }
+
+    private static Point2D nearestHelper(boolean orient, Node parent, Point2D target, Point2D favorite) {
+        if (parent == null)
+            return favorite;
+
+        if (parent.p.distanceSquaredTo(target) < favorite.distanceSquaredTo(target))
+            favorite = parent.p;
+
+        if (orient == VERT) {
+            Node first = target.x() < parent.p.x() ? parent.lb : parent.rt;
+            Node second = target.x() < parent.p.x() ? parent.rt : parent.lb;
+
+            favorite = nearestHelper(HORIZ, first, target, favorite);
+
+            if (second != null
+                    && favorite.distanceSquaredTo(target) > second.rect.distanceSquaredTo(target))
+                favorite = nearestHelper(HORIZ, second, target, favorite);
+        }
+        else {
+            Node first = target.y() < parent.p.y() ? parent.lb : parent.rt;
+            Node second = target.y() < parent.p.y() ? parent.rt : parent.lb;
+
+            favorite = nearestHelper(VERT, first, target, favorite);
+
+            if (second != null
+                    && favorite.distanceSquaredTo(target) > second.rect.distanceSquaredTo(target))
+                favorite = nearestHelper(VERT, second, target, favorite);
+        }
+
+        return favorite;
     }
 
     private static class Node {
@@ -103,8 +170,9 @@ public class KdTree {
         private Node lb;
         private Node rt;
 
-        public Node(Point2D p) {
+        public Node(Point2D p, RectHV rect) {
             this.p = p;
+            this.rect = rect;
         }
     }
 }
